7205888b27b8d943a57f97e764ac05db4a6a9ea2
m2dan <45355464+m2dan@users.noreply.github.com>
2018-11-27 09:33:25 +0100

    Durchschnittsberechnung der Noten&Punkte

---
+++ Durchschnittsberechnung.py
@@ -0,0 +1,39 @@
+# Berechnung der Noten und Punkte Durchschnitt
+
+print("gib dein erstes Fach ein:")
+fach1 = input()
+print("Gib deine Punkte ein:")
+punkte1 = int(input())
+
+print("gib dein zweites Fach ein:")
+fach2 = input()
+print("Gib deine Punkte ein:")
+punkte2 = int(input())
+
+print("gib dein drittes Fach ein:")
+fach3 = input()
+print("Gib deine Punkte ein:")
+punkte3 = int(input())
+
+print("gib dein viertes Fach ein:")
+fach4 = input()
+print("Gib deine Punkte ein:")
+punkte4 = int(input())
+
+print("gib dein fünftes Fach ein:")
+fach5 = input()
+print("Gib deine Punkte ein:")
+punkte5 = int(input())
+
+durchschnitt = (punkte1 + punkte2 + punkte3 + punkte4 + punkte5) / 5
+print("Noten Durschnitt:", durchschnitt)
+
+
+
+
+
+
+
+
+
+
