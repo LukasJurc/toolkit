4202d7fda9e36e00bed0fa2891930c6bd5c6ddf4
m2dan <45355464+m2dan@users.noreply.github.com>
2018-11-27 09:38:29 +0100

    Add files via upload

--- Durchschnittsberechnung.py
+++ Durchschnittsberechnung.py
@@ -1,31 +1,31 @@
 # Berechnung der Noten und Punkte Durchschnitt
 
-print("gib dein erstes Fach ein:")
-fach1 = input()
-print("Gib deine Punkte ein:")
+print("gib dein erstes Fach ein:")      # Eingabe eines Modul
+fach1 = input()                     
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte 
 punkte1 = int(input())
 
-print("gib dein zweites Fach ein:")
+print("gib dein zweites Fach ein:")     # Eingabe eines Modul
 fach2 = input()
-print("Gib deine Punkte ein:")
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte 
 punkte2 = int(input())
 
-print("gib dein drittes Fach ein:")
+print("gib dein drittes Fach ein:")     # Eingabe eines Modul
 fach3 = input()
-print("Gib deine Punkte ein:")
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte3 = int(input())
 
-print("gib dein viertes Fach ein:")
+print("gib dein viertes Fach ein:")     # Eingabe eines Modul
 fach4 = input()
-print("Gib deine Punkte ein:")
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte4 = int(input())
 
-print("gib dein fünftes Fach ein:")
+print("gib dein fünftes Fach ein:")     # Eingabe eines Modul
 fach5 = input()
-print("Gib deine Punkte ein:")
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte5 = int(input())
 
-durchschnitt = (punkte1 + punkte2 + punkte3 + punkte4 + punkte5) / 5
+durchschnitt = (punkte1 + punkte2 + punkte3 + punkte4 + punkte5) / 5    # Durchschnittsberechnung der Klausurpunkte
 print("Noten Durschnitt:", durchschnitt)
 
 
