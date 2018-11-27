acaf2b6c5ab1878b8b26e646a551e4be9561196f
m2dan <45355464+m2dan@users.noreply.github.com>
2018-11-27 13:08:25 +0100

    durchschnittsberechnung

--- durchschnittsberechnung.py
+++ durchschnittsberechnung.py
@@ -1,61 +1,116 @@
 # Berechnung der Noten und Punkte Durchschnitt
 
-print("gib dein erstes Fach ein:")      # Eingabe eines Modul
+print("Geben sie Bitte Ihr erstes Fach ein:")      # Eingabe des ersten Fach
 fach1 = input()
 while len(fach1)>40 or len(fach1)< 2or fach1.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung bitte ein Fach eingeben
     fach1 = input()
 print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte 
 punkte1 = int(input())
-while int(punkte1) >3 or int(punkte1) <0 or punkte1.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(min 1 and max 3)")
+while int(punkte1) >100 or int(punkte1) <0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
     punkte1 = int(input())
 
-print("gib dein zweites Fach ein:")     # Eingabe eines Modul
+print("Geben sie Bitte Ihr zweites Fach ein:")     # Eingabe des zweiten Fach
 fach2 = input()
 while len(fach2)>40 or len(fach2)< 2or fach2.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
     fach2 = input()
 print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte 
 punkte2 = int(input())
-while len(punkte2)>40 or len(punkte2)< 2 or punkte2.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(min 2 and max 40)")
+while int(punkte2)>100 or int(punkte2)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
     punkte2 = int(input())
 
-print("gib dein drittes Fach ein:")     # Eingabe eines Modul
+print("Geben sie Bitte Ihr drittes Fach ein:")     # Eingabe des dritten Fach
 fach3 = input()
 while len(fach3)>40 or len(fach3)< 2or fach3.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
     fach3 = input()
 print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte3 = int(input())
-while len(punkte3)>40 or len(punkte3)< 2 or punkte3.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(min 2 and max 40)")
+while int(punkte3)> 100 or int(punkte3)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
     punkte3 = int(input())
 
-print("gib dein viertes Fach ein:")     # Eingabe eines Modul
+print("Geben sie Bitte Ihr viertes Fach ein:")     # Eingabe des vierten Fach
 fach4 = input()
 while len(fach4)>40 or len(fach4)< 2or fach4.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
     fach4 = input()
 print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte4 = int(input())
-while len(punkte4)>40 or len(punkte4)< 2 or punkte4.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(min 2 and max 40)")
+while int(punkte4)> 100 or int(punkte4)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
     punkte4 = int(input())
 
-print("gib dein fünftes Fach ein:")     # Eingabe eines Modul
+print("Geben sie Bitte Ihr fünftes Fach ein:")     # Eingabe des fünften Fach
 fach5 = input()
 while len(fach5)>40 or len(fach5)< 2or fach5.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
     fach5 = input()
 print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
 punkte5 = int(input())
-while len(punkte5)>40 or len(punkte5)< 2 or punkte5.isspace():
-    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(min 2 and max 40)")
+while int(punkte5)> 100 or int(punkte5)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
     punkte5 = int(input())
 
-durchschnitt = (punkte1 + punkte2 + punkte3 + punkte4 + punkte5) / 5  # Durchschnittsberechnung der Klausurpunkte
+print("Geben sie Bitte Ihr sechstes Fach ein:")     # Eingabe des sechsten Fach
+fach6 = input()
+while len(fach6)>40 or len(fach6)< 2or fach6.isspace():
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
+    fach6 = input()
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
+punkte6 = int(input())
+while int(punkte6)> 100 or int(punkte6)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
+    punkte6 = int(input())
+    
+print("Geben sie Bitte Ihr siebtes Fach ein:")     # Eingabe des siebten Fach
+fach7 = input()
+while len(fach7)>40 or len(fach7)< 2or fach7.isspace():
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
+    fach7 = input()
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
+punkte7 = int(input())
+while int(punkte7)> 100 or int(punkte7)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
+    punkte7 = int(input())
+    
+print("geben sie Bitte Ihr achtes Fach ein:")     # Eingabe des achten Fach
+fach8 = input()
+while len(fach8)>40 or len(fach8)< 2or fach8.isspace():
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
+    fach8 = input()
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
+punkte8 = int(input())
+while int(punkte8)> 100 or int(punkte8)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
+    punkte8 = int(input())
+
+print("Geben sie Bitte Ihr neuntes Fach ein:")     # Eingabe des neunten Fach
+fach9 = input()
+while len(fach9)>40 or len(fach9)< 2or fach9.isspace():
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
+    fach9 = input()
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
+punkte9 = int(input())
+while int(punkte9)> 100 or int(punkte9)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
+    punkte9 = int(input())
+    
+print("Geben sie Bitte Ihr zehntes Fach ein:")     # Eingabe des zehnten Fach
+fach10 = input()
+while len(fach10)>40 or len(fach10)< 2or fach10.isspace():
+    print("Fehlermeldung!!!   Bitte geben sie ein Fach ein(min 2 and max 40)")   # Fehlermeldung  bitte ein Fach eingeben
+    fach10 = input()
+print("Gib deine Punkte ein:")          # Eingabe der KlausurPunkte
+punkte10 = int(input())
+while int(punkte10)> 100 or int(punkte10)< 0 or ():
+    print("Fehlermeldung!!!   Bitte geben sie ihre Punkte ein(zwischen 0 und 100)")   # Fehlermeldung bitte geben sie ihre Klausurpunkte ein
+    punkte5 = int(input())
+
+durchschnitt = (punkte1 + punkte2 + punkte3 + punkte4 + punkte5 + punkte6 + punkte7 + punkte8 + punkte9 + punkte10) / 10  # Durchschnittsberechnung der Klausurpunkte
 print("Klausurpunkte Durschnitt:", durchschnitt)
 
 
