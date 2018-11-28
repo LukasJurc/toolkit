ceceb4aaa76ef08ed5bd7220ae75126a357dc71b
samurai.codes <samurais.codes@gmail.com>
2018-11-28 10:48:22 +0100

    style: indentation corrections

--- Graphics.py
+++ Graphics.py
@@ -34,7 +34,6 @@ def create_window1():
             self.output.pack(side="top", fill="x", expand=True)
             self.submit.pack(side="right")
 
-
         def calculate(self):
         # get the value from the input widget, convert
         # it to an int, and do a calculation
@@ -59,60 +58,60 @@ def create_window1():
 def create_window2():
     
     class Example(tk.Frame):
-            def __init__(self, parent):
-                tk.Frame.__init__(self, parent)
-                root.geometry("800x600")
-                root.resizable(0,0)
-                root.title("GPB Grades")
-                root.option_add("*background", "lightgrey")
+        def __init__(self, parent):
+            tk.Frame.__init__(self, parent)
+            root.geometry("800x600")
+            root.resizable(0,0)
+            root.title("GPB Grades")
+            root.option_add("*background", "lightgrey")
 
-        #Entry 1, Window 2
+#Entry 1, Window 2
         
         # create a prompt, an input box, an output label,
         # and a button to do the computation
-                self.prompt = tk.Label(self, text="Your Course:", anchor="w")
-                self.entry = tk.Entry(self)
-                self.submit = tk.Button(self, text="Submit", command = self.calculate)
-                self.output = tk.Label(self, text="")
+            self.prompt = tk.Label(self, text="Your Course:", anchor="w")
+            self.entry = tk.Entry(self)
+            self.submit = tk.Button(self, text="Submit", command = self.calculate)
+            self.output = tk.Label(self, text="")
                 
 
         # lay the widgets out on the screen. 
-                self.prompt.place(x=30, y=0, width=300, height=50)
-                self.entry.place(x=30, y=60, width=300, height=50)
-                self.submit.place(x=30, y=120, width=300, height=50)
-                self.output.place(x=30, y=180, width=300, height=50)
+            self.prompt.place(x=30, y=0, width=300, height=50)
+            self.entry.place(x=30, y=60, width=300, height=50)
+            self.submit.place(x=30, y=120, width=300, height=50)
+            self.output.place(x=30, y=180, width=300, height=50)
                 
 
 #Entry 2, Window 2
-                self.prompt = tk.Label(self, text="Your Grade:", anchor="w")
-                self.entry = tk.Entry(self)
-                self.submit = tk.Button(self, text="Submit", command = self.calculate)
-                self.output = tk.Label(self, text="")
+            self.prompt = tk.Label(self, text="Your Grade:", anchor="w")
+            self.entry = tk.Entry(self)
+            self.submit = tk.Button(self, text="Submit", command = self.calculate)
+            self.output = tk.Label(self, text="")
 
         # lay the widgets out on the screen. 
-                self.prompt.place(x=400, y=0, width=300, height=50)
-                self.entry.place(x=400, y=60, width=300, height=50)
-                self.submit.place(x=400, y=120, width=300, height=50)
-                self.output.place(x=400, y=180, width=300, height=50)
+            self.prompt.place(x=400, y=0, width=300, height=50)
+            self.entry.place(x=400, y=60, width=300, height=50)
+            self.submit.place(x=400, y=120, width=300, height=50)
+            self.output.place(x=400, y=180, width=300, height=50)
 
-            def calculate(self):
+        def calculate(self):
         # get the value from the input widget, convert
         # it to an int, and do a calculation
-                try:
-                    i = int(self.entry.get())
-                    result = "%s*2=%s" % (i, i*2)
-                except ValueError:
-                    result = "Please enter digits only"
+            try:
+                i = int(self.entry.get())
+                result = "%s*2=%s" % (i, i*2)
+            except ValueError:
+                result = "Please enter digits only"
 
         # set the output widget to have our result
-                self.output.configure(text=result)
+            self.output.configure(text=result)
 
 # if this is run as a program (versus being imported),
 # create a root window and an instance of our example,
 # then start the event loop
     if __name__ == "__main__":
-            root = tk.Tk()
-            Example(root).pack(fill="both", expand=True)
+        root = tk.Tk()
+        Example(root).pack(fill="both", expand=True)
     
 
 
