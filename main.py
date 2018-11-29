b441ea245cdb062650f722104eccc936132ac67c
LukasJurc <45354980+LukasJurc@users.noreply.github.com>
2018-11-29 20:51:59 +0100

    simplified windows into modules for performance

--- main.py
+++ main.py
@@ -1,84 +1,8 @@
 import tkinter as tk
 import input_new as bb
 
-def window_one():
     
-    window_one = tk.Tk()
-    window_one.geometry("550x600")
-    window_one.resizable(0,0)
-    window_one.title("GPB Tools")
-    
-    label_one = tk.Label(window_one, text="Was möchten Sie suchen?")
-    label_one.place(x=20, y=5, width=80, height=30)
-    
-    entry_one = tk.Entry(window_one)
-    entry_one.place(x=130, y=5, width=300, height=30)
-    
-    button_three = tk.Button(window_one, text="Submit")
-    button_three.place(x=450, y=5, width=80, height=30)
-    
-    text_output = tk.Label(window_one, text="")
-    text_output.place(x=20, y=45, width=510, height=30)
-    
-    text_output_two = tk.Label(window_one,text= "ok")
-    text_output_two.place(x=20, y=85, width=510, height=500)
-    
-    window_one.mainloop()
-    
-def window_two():
-    
-    window_one = tk.Tk()
-    window_one.geometry("550x600")
-    window_one.resizable(0,0)
-    window_one.title("GPB Tools")
-    
-    label_one = tk.Label(window_one, text="Was möchten Sie suchen?")
-    label_one.place(x=20, y=5, width=80, height=30)
-    
-    entry_one = tk.Entry(window_one)
-    entry_one.place(x=130, y=5, width=300, height=30)
-    
-    button_three = tk.Button(window_one, text="Submit")
-    button_three.place(x=450, y=5, width=80, height=30)
-    
-    text_output = tk.Label(window_one, text="")
-    text_output.place(x=20, y=45, width=510, height=30)
-    
-    text_output_two = tk.Label(window_one,text= "ok")
-    text_output_two.place(x=20, y=85, width=510, height=500)
-    
-    window_one.mainloop()
-    
-def window_three():
-    bb.window_three()
-    
-def window_four():
-    
-    window_one = tk.Tk()
-    window_one.geometry("550x600")
-    window_one.resizable(0,0)
-    window_one.title("GPB Tools")
-    
-    label_one = tk.Label(window_one, text="Was möchten Sie suchen?")
-    label_one.place(x=20, y=5, width=80, height=30)
-    
-    entry_one = tk.Entry(window_one)
-    entry_one.place(x=130, y=5, width=300, height=30)
-    
-    button_three = tk.Button(window_one, text="Submit")
-    button_three.place(x=450, y=5, width=80, height=30)
-    
-    text_output = tk.Label(window_one, text="")
-    text_output.place(x=20, y=45, width=510, height=30)
-    
-    text_output_two = tk.Label(window_one,text= "ok")
-    text_output_two.place(x=20, y=85, width=510, height=500)
-    
-    window_one.mainloop()
-    
-
-def press_button_one():
-    window_one()
+def press_button_one():    window_one()
 
 
 def press_button_two():
@@ -86,7 +10,7 @@ def press_button_two():
 
 
 def press_button_three():
-    window_one()
+    bb.window_three()
 
 
 def press_button_four():
