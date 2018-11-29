c4d9e36d818c3cc42b7d87a63300d55d7b365158
Black <furkan.ismail.karakus@gmail.com>
2018-11-29 09:03:14 +0100

    Still testing

--- Fach + Punkte.py
+++ Fach + Punkte.py
@@ -1,35 +1,43 @@
-#Zuerst auf Deutsch
+# Start of the function
 
-text = "Hallo! In diesem Menü kannst du deine Fächer inklusive Punkte eintragen."
+counter = 3
+counter2 = 10
 
-laenge = len(text)
-print("*" + "*" * laenge + "*" + "*")
-print("*" + text +  "*")
-print("*" *2 + "*" * laenge + '*')
+print("Wie viele Fächer wollen sie eintragen?: ")
+number_answer = input()
 
-fächer = ["fach1", "fach2", "fach3", "fach4", "fach5", "fach6", "fach7", "fach8", "fach9", "fach10"]
-punkte = ["punkte1", "punkte2", "punkte3", "punkte4", "punkte5", "punkte6", "punkte7", "punkte8", "punkte9", "punkte10"]
+while int(counter)> int(number_answer) or int(counter2)< int(number_answer):
+    print("""Der Durchschnitt kann nur mit mindestens 3 Fächern berechnet werden.
+Bitte geben sie eine Zahl zwischen 3-10 ein.""")
+    number_answer = input()
+    
+if int(number_answer) <int(counter2) or int(number_answer) > int(counter):
+    print("OK, wir machen weiter.")
 
 
-for fächer and punkte in range (0,10):
-    input("Geben sie ein Fach ein: ")
+#fächer = ["fach1", "fach2", "fach3", "fach4", "fach5", "fach6", "fach7", "fach8", "fach9", "fach10"]
+#punkte = ["punkte1", "punkte2", "punkte3", "punkte4", "punkte5", "punkte6", "punkte7", "punkte8", "punkte9", "punkte10"]
+
+list_fächer = []
+list_punkte = []
+start0 = 0
+
+while start0 < int(number_answer):
+        fächer = input("Geben sie ein Fach ein: ")
+        list_fächer.insert(0,fächer)
+        punkte = input("Geben sie ihre Punktzahl ein: ")
+        list_punkte.insert(0,int(punkte))
+        start0 = start0 +1
+
+# first make a list out of the subjects
+
+#fächer[1] = input("Geben sie ein Fach ein: ")
+#punkte[1] = input("Geben sie ihre Punktzahl ein: ")
 
 
-#Fächer[0:9] = input("Geben sie ein Fach ein: ")
-#Punkte[0:9] = input("Geben sie ihre Punktzahl ein: ")
+#return (fächer1)
 
-#Fächer[1] = input("Geben sie ein Fach ein: ")
-#Punkte[1] = input("Geben sie ihre Punktzahl ein: ")
 
-#import pickle
-
-#l = [1,2,3,4]
-
-import json
-a = [1,2,3]
-with open('test.txt', 'w') as f:
-    f.write(json.dumps(a))
-
-#Now read the file back into a Python list object
-with open('test.txt', 'r') as f:
-    a = json.loads(f.read())
+#def fächer2(Schulfächer):
+    #fächer[1:9]
+#fächer =
