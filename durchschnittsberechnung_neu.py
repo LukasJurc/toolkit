b9aab1684cfb60e8fbc1b27cbaf286ac57d3f2b9
samurai.codes <samurais.codes@gmail.com>
2018-12-02 16:54:50 +0100

    Add files via upload

---
+++ durchschnittsberechnung_neu.py
@@ -0,0 +1,25 @@
+# Durchschnittsberechnung
+if len(list_punkte) == 3:
+    print("Durchschnittspunkte", ( list_punkte[0] + list_punkte[1] + list_punkte[2])/3)
+    
+elif len(list_punkte) == 4:
+    print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3])/4)
+
+elif len(list_punkte) == 5:
+    print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4])/5)
+    
+elif len(list_punkte) == 6:
+     print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4] + list_punkte[5])/6)
+
+elif len(list_punkte) == 7:
+    print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4] + list_punkte[5] + list_punkte[6])/7)
+
+elif len(list_punkte) == 8:
+    print(("Durchschnittspunkte", list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4] + list_punkte[5] + list_punkte[6] + list_punkte[7])/8)
+
+elif len(list_punkte) == 9:
+    print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4] + list_punkte[5] + list_punkte[6] + list_punkte[7] + list_punkte[8])/9)
+
+elif len(list_punkte) == 10:
+    print("Durchschnittspunkte", (list_punkte[0] + list_punkte[1] + list_punkte[2] + list_punkte[3] + list_punkte[4] + list_punkte[5] + list_punkte[6] + list_punkte[7] + list_punkte[8] + list_punkte[9])/10)
+
